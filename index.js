49b2bcb2f6989dd3a8a9f25bd9c99eef67bbdf34
Rajendra Khope <research@iocare.in>
2022-03-12 20:43:14 +0530

    updates

--- index.js
+++ index.js
@@ -1,27 +1,65 @@
 #! /usr/bin/env node
-const conf = new (require('conf'))()
-const chalk = require('chalk')
-function list () {
-    const todoList = conf.get('todo-list')
-    if (todoList && todoList.length) {
-        console.log(
-            chalk.blue.bold('Tasks in green are done. Tasks in yellow are still not done.')
-        )
-        todoList.forEach((task, index) => {
-            if (task.done) {
-                console.log(
-                    chalk.greenBright(`${index}. ${task.text}`)
-                )
-            } else {
-                console.log(
-                    chalk.yellowBright(`${index}. ${task.text}`)
-                )
-            }
-        })
-    } else {
-        console.log(
-            chalk.red.bold('You don\'t have any tasks yet.')
-        )
-    }
-}
-module.exports = list
+// var shell = require("shelljs");
+// const { Command } = require('commander');
+// const program = new Command();
+
+// program
+//   .name('string-util')
+//   .description('Confluxsys Container Management Utility - CoCoMan')
+//   .version('1.0.0')
+
+
+//   program.command('split')
+//   .description('Confluxsys Container Management Utility - CoCoMan')
+//   .argument('<string>', 'string to split')
+//   .option('--first', 'display just the first substring')
+//   .option('-s, --separator <char>', 'separator character', ',')
+//   .action((str, options) => {
+//     const limit = options.first ? 1 : undefined;
+//     console.log(str.split(options.separator, limit));
+//   });
+
+// program.parse();
+
+// //const options = program.opts();
+// ///const limit = options.first ? 1 : undefined;
+// //console.log(program.args[0].split(options.separator, limit));
+// console.log('Test');
+
+// shell.exec("echo shell.exec works");
+
+var inquirer = require('inquirer');
+const questions = [
+  {
+    type: 'input',
+    name: 'first_question',
+    message: 'Question with filtering and validating text',
+    validate: async () => {
+      await new Promise((r) => setTimeout(r, 3000));
+      return true;
+    },
+    filter: async (answer) => {
+      await new Promise((r) => setTimeout(r, 3000));
+      return `filtered${answer}`;
+    },
+    filteringText: 'Filtering your answer...',
+    validatingText: 'Validating what you wrote...',
+  },
+  {
+    type: 'input',
+    name: 'second_question',
+    message: 'Question without filtering and validating text',
+    validate: async () => {
+      await new Promise((r) => setTimeout(r, 3000));
+      return true;
+    },
+    filter: async (answer) => {
+      await new Promise((r) => setTimeout(r, 3000));
+      return `filtered${answer}`;
+    },
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  console.log(JSON.stringify(answers, null, '  '));
+});
